444ff24f60f0460dcf705f2ce0a8f5cbf2140e69
Patsagorn <patsagorn_y@outlook.com>
2023-06-03 01:24:35 +0700

    add build option

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,10 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+		}),
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH
 		}
